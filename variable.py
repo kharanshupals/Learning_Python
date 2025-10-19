74ada47e667565d23d448cbeb7fcdb4de980a4e6
kharanshumac <kharanshups@gmail.com>
2025-10-19 20:40:20 +0530

    Variables

---
+++ variable.py
@@ -0,0 +1,6 @@
+#A variable is a named storage location in a program.
+#It holds a value which can change (or “vary”) during the execution of the program.
+
+age = 18
+percentage = 85.4
+name = "Aryan"
